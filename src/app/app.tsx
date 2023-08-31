3d2963dd5d6b220431adb331e516513ce856d8f3
ghislaingenay <ghislain.gny@gmail.com>
2023-08-31 11:20:29 +0700

    initialize typescript

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import { Divider } from '@lib/Divider';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-redilib" />
+      <Divider>App</Divider>
+      {/* <NxWelcome title="react-redilib" /> */}
     </div>
   );
 }
