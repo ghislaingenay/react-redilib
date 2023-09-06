ad8247ba5a59905a23bb090b0e14e5517739e55b
ghislaingenay <ghislain.gny@gmail.com>
2023-09-06 09:40:50 +0700

    component:create toggle loading wt twMerge

--- src/lib/Toggle/Toggle.stories.tsx
+++ src/lib/Toggle/Toggle.stories.tsx
@@ -1,5 +1,6 @@
 import type { StoryObj, Meta } from '@storybook/react';
 import { Toggle } from './Toggle';
+import { useEffect, useState } from 'react';
 
 const meta: Meta<typeof Toggle> = {
   title: 'Components/Toggle',
@@ -11,8 +12,43 @@ export default meta;
 
 type Story = StoryObj<typeof Toggle>;
 
+const LoadingToggle = () => {
+  const [isChecking, setIsChecking] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsChecking((prev) => !prev);
+    }, 2000);
+  }, [isChecking]);
+
+  // add random color system nd give user teh choice
+
+  return (
+    <div className="grid h-screen relative">
+      <div className="place-self-center">
+        <Toggle
+          isToggled={isChecking}
+          className="bg-purple-500 h-4 w-10 scale-[400%] py-10 px-6 border-4 border-purple-500 rounded-full checked:bg-orange-500"
+        />
+      </div>
+      {/* <div className="place-self-center absolute rotate-90">
+        <Toggle
+          isToggled={!isChecking}
+          className="bg-purple-500 h-5 w-14 scale-[400%] checked:bg-orange-500"
+        />
+      </div> */}
+    </div>
+  );
+};
+
 export const Base: Story = {
   render(args) {
     return <Toggle {...args} />;
+  },
+};
+
+export const ToggleLoading: Story = {
+  render() {
+    return <LoadingToggle />;
   },
 };
--- src/lib/Toggle/Toggle.tsx
+++ src/lib/Toggle/Toggle.tsx
@@ -1,14 +1,20 @@
+import { twMerge } from 'tailwind-merge';
+
 export type ToggleProps = {
   isToggled?: boolean;
   onClick?: () => void;
+  className?: string;
 };
 
-function Toggle({ isToggled = false, onClick }: ToggleProps) {
+function Toggle({ isToggled = false, onClick, className = '' }: ToggleProps) {
   const checkbox = isToggled ? { checked: true } : { checked: false };
   return (
     <input
       type="checkbox"
-      className="form-control toggle toggle-lg rounded-full toggle-success "
+      className={twMerge(
+        'form-control toggle toggle-lg rounded-full toggle-success',
+        className
+      )}
       onClick={onClick}
       {...checkbox}
     />
